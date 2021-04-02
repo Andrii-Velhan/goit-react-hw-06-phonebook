188f33492615c8a40bc7bff186160aeb90d6fda9
Andrii-Velhan <velganaa@gmail.com>
2021-04-02 22:42:23 +0300

    add store

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './redux/store'
 import reportWebVitals from './reportWebVitals';
 
+console.log(store);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
